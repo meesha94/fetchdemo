d112ab3b847a51e719e5e7b5941514867eb47fa0
meesha94 <95078931+meesha94@users.noreply.github.com>
2022-01-25 11:39:47 +0000

    added useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
 import './App.css';
+import  { useState, useEffect } from "react";
 
 //https://cat-fact.herokuapp.com/facts/random
 
 const App = () => {
+  const [fact, setFact] = useState({})
+
+  useEffect(() => {
+    handleFetch()
+  }, [])
+
+  useEffect(() => {
+    console.log("the fact changed")
+
+  },[fact])
+
+
   const handleFetch = async () => {
     const response = await fetch("https://cat-fact.herokuapp.com/facts/random")
-    console.log(response)
+    const data = await response.json()
+   
+    
+    setFact(data)
   };
   
   return (
     <div>
-      <h1>Hello world</h1>
+      <h1>Hello API's!</h1>
       <button onClick={handleFetch}>Run the fetch request!</button>
+      <p>{fact.text}</p>
     </div>
   )
 };
